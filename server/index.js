9711c645b79abab4d56203759bb0a5b44f6a32bb
EricWeiner <Eweiner11@gmail.com>
2020-07-29 14:58:32 -0400

    initialized repo

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+
+app.use(bodyParser.json())
+app.use(express.static('./client/dist'))
+
+let port = 3000;
+
+app.listen(port, (err)=>{
+if(err){
+    console.log(err);
+}else{
+    console.log(`listening on port: ${port}`)
+}
+})
