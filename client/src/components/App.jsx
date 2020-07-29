d6fa6f9239d6e608606e3ffc62de47881e06cbca
Eric Weiner <Eweiner11@gmail.com>
2020-07-29 15:39:50 -0400

    basic app setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,34 @@
 import React from "react"
+import WelcomeScreen from "./WelcomeScreen.jsx"
 
 
 class App extends React.Component {
 constructor(props){
     super(props);
+    this.state={
+        view:'welcome screen'
+    }
+    this.renderView=this.renderView.bind(this);
+    this.changeView= this.changeView.bind(this);
 
 }
-
+renderView(){
+   if(this.state.view==="welcome screen"){
+       return <WelcomeScreen changeView = {this.changeView}/>
+   }else if( this.state.view = "round setup"){
+       return <div>round setup goes here</div>
+   }
+}
+changeView(view){
+this.setState({'view':view});
+}
 
 render(){
 
     return(
-        <div>GOLF APP</div>
+        <div>
+        {this.renderView()}
+       </div>
     )
 }
 }
---
+++ client/src/components/WelcomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+var WelcomeScreen = (props) =>(
+
+<div> <button onClick = {() =>props.changeView("round setup")}>Play</button></div>
+
+
+
+
+
+)
+
+export default WelcomeScreen;
+
+
+
