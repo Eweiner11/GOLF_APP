a1032adfea36deeef6dfcc0c268a591f2b708efe
EricWeiner <Eweiner11@gmail.com>
2020-07-30 02:25:35 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+const config = {
+    entry: './client/src/index.js',
+    output: {
+      path: path.resolve(__dirname, './client/dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [
+        '.js',
+        '.jsx'
+      ]
+    }
+  };
+  
+  module.exports = config;
